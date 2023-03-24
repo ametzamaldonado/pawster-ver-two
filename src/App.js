ef0f5c9cad726f42a460b543b66f4a809334ec84
ametzamaldonado <ametza.maldonado@gmail.com>
2023-03-23 22:05:28 -0400

    added inital card functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import TinderContainer from './components/TinderContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box-a">
+        <h1>Hi</h1>
+      </div>
+      <div className="box-b">
+        <TinderContainer />
+      </div>
     </div>
   );
 }
---
+++ src/components/TinderContainer.js
@@ -0,0 +1,86 @@
+// https://www.geeksforgeeks.org/how-to-create-tinder-card-swipe-gesture-using-react-and-framer-motion/
+import React, { useState } from "react";
+import { motion, useMotionValue, useTransform, useAnimation } from "framer-motion";
+
+// Some styling for the card
+const style = {
+  backgroundImage: "URL(https://img.icons8.com/color/452/GeeksforGeeks.png)",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "contain",
+  backgroundColor: "#55ccff",
+  backgroundPosition: 'center',
+  boxShadow: "5px 10px 18px #888888",
+  borderRadius: 10,
+  height: 400,
+  width: 200,
+  margin: "0 auto", // Add this line to center the card horizontally
+};
+
+export default function TinderContainer() {
+    const [motionDirection, setMotionDirection] = useState(null);
+  // To move the card as the user drags the cursor
+  const motionValue = useMotionValue(0);
+
+  // To rotate the card as the card moves on drag
+  const rotateValue = useTransform(motionValue, [-200, 200], [-50, 50]);
+
+  // To decrease opacity of the card when swiped
+  // on dragging card to left(-200) or right(200)
+  // opacity gradually changes to 0
+  // and when the card is in center opacity = 1
+  const opacityValue = useTransform(
+    motionValue,
+    [-200, -150, 0, 150, 200],
+    [0, 1, 1, 1, 0]
+  );
+
+  // Framer motion animation hook
+//   const animControls = useAnimation();
+const animControls = useAnimation({
+    transition: { duration: .3 },
+  });
+
+  const handleDragEnd = (event, info) => {
+    if (Math.abs(info.point.x) <= 150) {
+      animControls.start({ x: 0 });
+    } else {
+      setMotionDirection(info.point.x < 0 ? "left" : "right");
+      animControls.start({ x: info.point.x < 0 ? -200 : 200 });
+    }
+  };
+
+  const handleSwipeLeft = () => {
+    setMotionDirection("left");
+    motionValue.set(-200);
+    animControls.start({ x: -200, opacity: 0 });
+  };
+
+  const handleSwipeRight = () => {
+    setMotionDirection("right");
+    motionValue.set(200);
+    animControls.start({ x: 200, opacity: 0 });
+  };
+
+  return (
+    <>
+      <motion.div
+        drag="x"
+        dragConstraints={{ left: -300, right: 300 }}
+        style={{
+          ...style,
+          x: motionValue,
+          rotate: rotateValue,
+          opacity: opacityValue,
+        }}
+        onDragEnd={handleDragEnd}
+      />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={handleSwipeLeft}>Swipe Left</button>
+        <button onClick={handleSwipeRight}>Swipe Right</button>
+      </div>
+      <p style={{ textAlign: "center" }}>
+        Last swipe direction: {motionDirection}
+      </p>
+    </>
+  );
+};
